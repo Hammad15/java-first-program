8ec384db6064fcc1bd04284058963c3e4ba6e56a
Mohammed Hammad <mohammed.hammad@revature.net>
2022-04-07 11:52:01 -0400

    Completed first 10 tasks

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,38 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+
+    public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter you name");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+        System.out.println("enter the loan term (in years)");
+
+        int loanTerm = scanner.nextInt();
+
+        float bestRate = getRates(loanTerm);
+
+        if (bestRate == 0.0f)
+            System.out.println("No available rates for term: " + loanTerm + " years");
+        else
+            System.out.println("Best Available Rate: " + getRates(loanTerm) + "%");
+
+        scanner.close();
+    }
+
+    public static float getRates(int loanTermInYears) {
+
+        return bestRates.getOrDefault(loanTermInYears, 0.0f);
+    }
+}
